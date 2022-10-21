dc9458530793058d798e60310925a261f0171cf0
Cameron Wonchoba <wonch002@umn.edu>
2022-10-21 00:54:23 -0600

    Update names of data_processing functions

--- city_explorer/datasets/__init__.py
+++ city_explorer/datasets/__init__.py
@@ -5,7 +5,7 @@ import os
 CACHE_FOLDER = os.path.join(os.path.dirname(__file__), "cache")
 
 from .data_processing import (
-    load_uscities as uscities,
-    load_income_microdata as income_microdata,
-    load_rent_microdata as rent_microdata,
+    load_uscities,
+    load_income_microdata,
+    load_rent_microdata,
 )
