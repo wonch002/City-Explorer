0d00a007dcb2dd2d6ede3f86c8dd98eebed3c320
Cameron Wonchoba <wonch002@umn.edu>
2022-11-29 10:42:25 -0700

    Add support for MacOS

--- city_explorer/tabpy_loader.py
+++ city_explorer/tabpy_loader.py
@@ -1,20 +1,49 @@
+"""Tabpy functions for Tableau."""
 import os
+import socket
+
+import platform
+import time
 from tabpy.tabpy_tools.client import Client
 
-# Starting Tabpy Server - This should open a terminal with a tabpy instance
-###----NEED TO VALIDATE FUNCTIONALITY ON MACOS----###
-assert os.system("start cmd /c tabpy") == 0, "Tabpy server failed to start!"
+
+def start_tabpy_server():
+    """Start the Tabpy server by opening a terminal and running `Tabpy`."""
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result = sock.connect_ex(("localhost", 9004))
+    # Only start if the server is not already running
+    if result != 0:
+        if platform.system() == "Windows":
+            assert os.system("start cmd /c tabpy") == 0, "Tabpy server failed to start!"
+        elif platform.system() == "Darwin":
+            assert (
+                os.system(
+                    """
+                    osascript -e 'tell app "Terminal" to do script "tabpy"'
+                    """
+                )
+                == 0
+            ), "Tabpy server failed to start!"
+        else:
+            raise NotImplementedError(f"{platform.system()} is not supported.")
+        time.sleep(1)  # Give the server time to fully start
+    else:
+        print("Server already running.")
+
+
+start_tabpy_server()
 
 # Defining Client
-client = Client('http://localhost:9004/')
+client = Client("http://localhost:9004/")
 
 
 # Defining Example Add Function
 def add(x, y):
     """Adds two numbers together using numpy.add()"""
     import numpy as np
+
     return np.add(x, y).tolist()
 
 
 # Deploy Example Add Function
-client.deploy('add', add, "Adds together two inputs, x and y", override=True)
+client.deploy("add", add, "Adds together two inputs, x and y", override=True)
