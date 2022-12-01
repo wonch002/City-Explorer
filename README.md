e5caf099236856e926d9274d2bc6a452d387e426
Cameron Wonchoba <wonch002@umn.edu>
2022-11-30 22:27:25 -0700

    Merge

--- README.md
+++ README.md
@@ -14,3 +14,67 @@ Ankit Anand, Matthew Tate, Krishna Tatineni, Cameron Wonchoba
 
 `pip install -r requirements.txt`
 
+
+
+
+# Final Todos:
+
+Timeline:
+    Thursday - 
+    Friday - Due date 
+    Saturday - Grace period day 1
+    Sunday - Grace period day 2 (Submit project)
+
+
+## Cameron
+
+- Create flask (FastAPI?) API for predicting similar cities
+    * Create demo code to hit the API
+    * Explore deploying to AWS?
+- Create README.txt with instructions
+    * Get code running and server running
+    * Open Tableau
+- Review Poster & Report
+- Create 3 minute video
+- Create team162final.zip
+    * README.txt
+    * DOC
+    * CODE
+
+## Matt
+
+- Clustering analysis
+- Tablaeu & Tabpy to flask connection
+    * This will connect all sliders to code
+- Review Poster & Report
+- Create 3 minute video
+- Upload video to YouTube
+
+## Kris
+
+- Create draft of final report. Ready for review by Saturday.
+- Get poster to almost finished state by Friday.
+    * Needs input from Cameron, Matt & Ankit by EOD Thursday.
+        - Your approaches (algorithm and interactive visualization): 
+            * 5% What are they?
+            * 5% How do they work? 
+            * 5% Why do you think they can effectively solve your problem (i.e., what is the intuition behind your approaches)? 
+            * 5% What is new in your approaches?
+        - Experiments and results: 
+            * 5% How did you evaluate your approaches? 
+            * 10% What are the results? 
+            * 10% How do your methods compare to other methods?
+ - Create 3 minute video
+ - Execute instructions from README.txt
+
+## Ankit
+
+- Finish Tablaeu dashboard
+- Create one calculated field called `selected city flag`
+    * Set to 1 (display the city) or 0 (do not display the city)
+    * This flag represents whether the city is recommended
+    * city_id level
+- Create README.txt with instructions
+    - How to use the dashboard
+- Review Poster & Report
+- Create 3 minute video
--- city_explorer/tabpy_loader.py
+++ city_explorer/tabpy_loader.py
@@ -1,7 +1,12 @@
 """Tabpy functions for Tableau."""
 import os
 import socket
+
 from typing import Callable
+
+from typing import Callable, Dict, List
+from flask import Flask, request
+
 
 import platform
 import time
@@ -73,6 +78,115 @@ class SimilarCitiesClient(Client):
 
 # Start our client
 client = SimilarCitiesClient()
+
+
+def similar_cities_tabpy(
+    cities: List[int],
+    city_id: int,
+    occupation_title: str,
+    population: float,
+    population_denisty: float,
+    age: float,
+    sex: float,
+    rental_prices: float,
+    house_prices: float,
+    affordability: float,
+    political_party: float,
+    winter_temperature: float,
+    spring_temperature: float,
+    summer_temperature: float,
+    fall_temperature: float,
+    precipitation: float,
+    snowfall: float,
+    education: float,
+) -> Dict[str, float]:
+    """TabPy for similar cities."""
+    import requests
+    import pandas as pd
+
+    HOSTNAME = "localhost"
+    FLASK_PORT = 5001
+
+    response = requests.get(
+        url=f"http://{HOSTNAME}:{FLASK_PORT}/predict_similar_cities/",
+        params=dict(
+            city_id=city_id,
+            occupation_title=occupation_title,
+            population=population,
+            population_denisty=population_denisty,
+            age=age,
+            sex=sex,
+            rental_prices=rental_prices,
+            house_prices=house_prices,
+            affordability=affordability,
+            political_party=political_party,
+            winter_temperature=winter_temperature,
+            spring_temperature=spring_temperature,
+            summer_temperature=summer_temperature,
+            fall_temperature=fall_temperature,
+            precipitation=precipitation,
+            snowfall=snowfall,
+            education=education,
+        ),
+    )
+    # result has the following structure is in this form
+    # {
+    #   city_id (str): similarity_score (float),
+    #   ...: ...
+    # }
+
+    result = response.json()
+
+    # index = city_id, value = similarity_score
+    filtered_cities = pd.Series(result).loc[cities]
+    ordered_similarity_scores = filtered_cities.values.tolist()
+
+    return ordered_similarity_scores
+
+
+def start_tabpy():
+    """Set up tabpy and deploy neccessary functions."""
+    client = SimilarCitiesClient()
+
+    # Deploy the neccessary functions
+    client.deploy(func=similar_cities_tabpy)
+
+
+# Deploy a flask application to do the heavy lifting.
+app = Flask(__name__)
+
+
+@app.route("/predict_similar_cities/", methods=["GET"])
+def predict_similar_cities():
+    """End point for predicting similar cities."""
+    city_id = int(request.args.get("city_id"))
+    occupation_title = str(request.args.get("occupation_title"))
+    sliders = [
+        float(request.args.get("population", default=1.0)),
+        float(request.args.get("population_denisty", default=1.0)),
+        float(request.args.get("age", default=1.0)),
+        float(request.args.get("sex", default=1.0)),
+        float(request.args.get("rental_prices", default=1.0)),
+        float(request.args.get("house_prices", default=1.0)),
+        float(request.args.get("affordability", default=1.0)),
+        float(request.args.get("political_party", default=1.0)),
+        float(request.args.get("winter_temperature", default=1.0)),
+        float(request.args.get("spring_temperature", default=1.0)),
+        float(request.args.get("summer_temperature", default=1.0)),
+        float(request.args.get("fall_temperature", default=1.0)),
+        float(request.args.get("precipitation", default=1.0)),
+        float(request.args.get("snowfall", default=1.0)),
+        float(request.args.get("education", default=1.0)),
+    ]
+
+    predictions = similar_cities.predict_similar_cities(
+        city_id=city_id,
+        occupation_title=occupation_title,
+        sliders=sliders,
+    )
+
+    return predictions.to_json()
+
 
 # Deploy the neccessary functions
 client.deploy(func=add)
